a672d89a807b2a14affd34e5c5cd0ab5293b5cb6
luongdao <luong.dao@pionero.io>
2023-10-27 10:16:43 +0700

    add health check using terminus

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,38 @@
-import { Controller } from '@nestjs/common';
-import { HealthBaseController } from './base/health.controller.base';
-import { HealthService } from './health.service';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import { RedisHealthIndicator } from '@liaoliaots/nestjs-redis-health';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, PrismaHealthIndicator } from '@nestjs/terminus';
+import { Redis } from 'ioredis';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('health')
-export class HealthController extends HealthBaseController {
-  constructor(protected readonly healthService: HealthService) {
-    super(healthService);
+export class HealthController {
+  constructor(
+    protected readonly healthService: HealthCheckService,
+    @InjectRedis() protected readonly redis: Redis,
+    protected prismaIndicator: PrismaHealthIndicator,
+    protected readonly redisIndicator: RedisHealthIndicator,
+    protected readonly prismaService: PrismaService,
+  ) {}
+
+  @Get('live')
+  healthLive() {
+    return this.healthService.check([]);
+  }
+
+  @Get('ready')
+  async healthReady() {
+    return this.healthService.check([
+      () =>
+        this.redisIndicator.checkHealth('redis', {
+          type: 'redis',
+          client: this.redis,
+          timeout: 2000,
+        }),
+      () =>
+        this.prismaIndicator.pingCheck('prisma', this.prismaService, {
+          timeout: 2000,
+        }),
+    ]);
   }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
-import { HealthService } from './health.service';
+import { TerminusModule } from '@nestjs/terminus';
+import { RedisHealthModule } from '@liaoliaots/nestjs-redis-health';
 
 @Module({
+  imports: [TerminusModule, RedisHealthModule],
   controllers: [HealthController],
-  providers: [HealthService],
+  providers: [],
 })
 export class HealthModule {}
