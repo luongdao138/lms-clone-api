e44d608a3be4b40aa100946aeb4957f8f643f266
luongdao <luong.dao@pionero.io>
2023-10-29 20:51:41 +0700

    update jwt guard

--- src/app/auth/guards/gql-jwt.guard.ts
+++ src/app/auth/guards/gql-jwt.guard.ts
@@ -1,4 +1,4 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Type } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
@@ -13,30 +13,46 @@ export class GqlJwtAuthGuard extends AuthGuard('jwt') {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = this.getRequest(context);
+    const handler = context.getHandler();
+    const resolverClass = context.getClass();
+
+    // check if this route is public => just let it pass
+    const isMethodPublic = this.reflector.get<boolean>(PUBLIC_KEY, handler);
+    const isClassPublic = this.reflector.get<boolean>(
+      PUBLIC_KEY,
+      resolverClass,
+    );
+    if (isMethodPublic || isClassPublic) return true;
+
     // this will invoke jwt strategy we defined before
     const isInitialValid = await super.canActivate(context);
     if (!isInitialValid) return false;
 
-    const req = this.getRequest(context);
-    const handler = context.getHandler();
-    const currentUser = req.user;
-
-    // check if this route is public => just let it pass
-    const isPublic = this.reflector.get<boolean>(PUBLIC_KEY, handler);
-    if (isPublic) return true;
-
-    return this.canActivateRoles(handler, currentUser);
+    return this.canActivateRoles(handler, resolverClass, req.user);
   }
 
   canActivateRoles(
     // eslint-disable-next-line @typescript-eslint/ban-types
     handler: Function,
+    resolverClass: Type<any>,
     currentUser: User,
   ): boolean {
-    const expectedRoles = this.reflector.get<string[]>(USER_ROLE_KEY, handler);
-    if (!expectedRoles) return false;
+    const expectedHandlerRoles = this.reflector.get<string[]>(
+      USER_ROLE_KEY,
+      handler,
+    );
+    const expectedClassRoles = this.reflector.get<string[]>(
+      USER_ROLE_KEY,
+      resolverClass,
+    );
+    if (!expectedHandlerRoles && !expectedClassRoles) return false;
 
-    return expectedRoles.includes(currentUser.role);
+    if (expectedHandlerRoles) {
+      return expectedHandlerRoles.includes(currentUser.role);
+    }
+
+    return expectedClassRoles.includes(currentUser.role);
   }
 
   getRequest(context: ExecutionContext) {
