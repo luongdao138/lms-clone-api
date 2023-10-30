d041dbc0a103538da64b766e1cde7cb3d7faddb1
luongdao <luong.dao@pionero.io>
2023-10-30 14:04:54 +0700

    add global to redis

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RedisModule as LiaoRedisModule } from '@liaoliaots/nestjs-redis';
 import { RedisOptionsService } from 'src/nest/providers/RedisOptions.service';
 
+@Global()
 @Module({
   imports: [
+    LiaoRedisModule.forRootAsync({
+      useClass: RedisOptionsService,
+    }),
+  ],
+  exports: [
     LiaoRedisModule.forRootAsync({
       useClass: RedisOptionsService,
     }),
