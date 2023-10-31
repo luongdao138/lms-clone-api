94256d19fd455164e3d6fce4ea571dd80eaefaff
luongdao <luong.dao@pionero.io>
2023-10-31 14:18:16 +0700

    add nginx config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,12 @@ module.exports = {
       name: 'prisma-studio',
       script: 'yarn db:studio',
       watch: './prisma',
+      ignore_watch: ['node_modules', '.git', '*.log'],
     },
     {
       name: 'dev-server',
       script: 'yarn start:dev',
+      ignore_watch: ['node_modules', '.git', '*.log'],
     },
   ],
 };
